aa3b240eaed9c5687ce9dc8157a1b531a16dac52
Carl Hain <crhain@gmail.com>
2019-03-28 10:11:24 -0500

    Change position of catAnimation from bottom to top

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -66,7 +66,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       builder: (context, child) {
         return Positioned(
           child: child,
-          bottom: catAnimation.value,
+          top: catAnimation.value,
           right: 0.0,
           left: 0.0,
         );
