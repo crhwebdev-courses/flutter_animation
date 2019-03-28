da2999bf927e31b7354bd841ab667b05eb8df8e3
Carl Hain <crhain@gmail.com>
2019-03-28 09:26:50 -0500

    Add flag for debuging layout lines to main.dart; it is currently set to false

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 
+import 'package:flutter/rendering.dart';
+
 void main() {
+  //enables layout lines for debuging layout
+  // if set to true
+  debugPaintSizeEnabled = false;
+
   runApp(App());
 }
